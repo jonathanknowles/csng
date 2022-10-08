aa1f6eb9ee1b7ac7ca0f75645db357aa4afc4754
Jonathan Knowles <mail@jonathanknowles.net>
2022-10-08 02:59:01 +0000

    Add types `Balance` and `Coin`.

---
+++ src/internal/Algebra/Partition.hs
@@ -0,0 +1,139 @@
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{- HLINT ignore "Use camelCase" -}
+
+module Algebra.Partition
+    where
+
+import Prelude
+
+import Data.List.NonEmpty
+    ( NonEmpty (..) )
+import Data.Maybe
+    ( mapMaybe )
+import Data.Monoid
+    ( Sum (..) )
+import Data.Proxy
+    ( Proxy )
+import Numeric.Natural
+    ( Natural )
+import Numeric.Natural.Extra
+    ( partitionNatural )
+import Test.QuickCheck
+    ( Arbitrary
+    , Gen
+    , Property
+    , Testable
+    , arbitrary
+    , checkCoverage
+    , forAllShrink
+    , property
+    , shrink
+    )
+import Test.QuickCheck.Classes
+    ( Laws (..) )
+
+import qualified Data.Foldable as F
+import qualified Data.List.NonEmpty as NE
+
+--------------------------------------------------------------------------------
+-- Class
+--------------------------------------------------------------------------------
+
+class Partition a where
+
+    partition
+        :: a -> NonEmpty a -> (a, NonEmpty a)
+    partitionMaybe
+        :: a -> NonEmpty a -> Maybe (NonEmpty a)
+
+    default partition
+        :: Monoid a
+        => a -> NonEmpty a -> (a, NonEmpty a)
+    partition a as = case partitionMaybe a as of
+        Nothing -> (a, mempty <$ as)
+        Just bs -> (mempty, bs)
+
+    default partitionMaybe
+        :: (Eq a, Monoid a)
+        => a -> NonEmpty a -> Maybe (NonEmpty a)
+    partitionMaybe a as = case partition a as of
+       (b, bs) | b == mempty -> Just bs
+       _ -> Nothing
+
+--------------------------------------------------------------------------------
+-- Laws
+--------------------------------------------------------------------------------
+
+partitionLaw_length :: Partition a => a -> NonEmpty a -> Bool
+partitionLaw_length a as =
+    length (snd (partition a as)) == length as
+
+partitionLaw_sum :: (Eq a, Monoid a, Partition a) => a -> NonEmpty a -> Bool
+partitionLaw_sum a as =
+    F.fold (uncurry NE.cons (partition a as)) == a
+
+--------------------------------------------------------------------------------
+-- Instances
+--------------------------------------------------------------------------------
+
+instance Partition (Sum Natural) where
+    partitionMaybe a as =
+        fmap Sum <$> partitionNatural (getSum a) (getSum <$> as)
+
+deriving via Sum Natural instance Partition Natural
+
+--------------------------------------------------------------------------------
+-- Testing
+--------------------------------------------------------------------------------
+
+partitionLaws
+    :: forall a.
+        ( Arbitrary a
+        , Eq a
+        , Monoid a
+        , Partition a
+        , Show a
+        )
+    => Proxy a
+    -> Laws
+partitionLaws _ = Laws "Partition"
+    [ ( "Length"
+      , makeProperty partitionLaw_length)
+    , ( "Sum"
+      , makeProperty partitionLaw_sum)
+    ]
+  where
+    makeProperty :: (a -> NonEmpty a -> Bool) -> Property
+    makeProperty =
+        property . forAllShrink genWeights shrinkWeights . makePropertyInner
+      where
+        genWeights :: Gen (NonEmpty a)
+        genWeights = (:|) <$> arbitrary <*> arbitrary
+
+        shrinkWeights :: NonEmpty a -> [NonEmpty a]
+        shrinkWeights = mapMaybe NE.nonEmpty . shrink . NE.toList
+
+    makePropertyInner
+        :: (a -> NonEmpty a -> Bool)
+        -> NonEmpty a
+        -> (a -> Property)
+    makePropertyInner condition weights value =
+        checkCoverage $
+        buildCoverage value weights result $
+        condition value weights
+      where
+        result = partition value weights
+
+    buildCoverage
+        :: Testable prop
+        => a
+        -> NonEmpty a
+        -> (a, NonEmpty a)
+        -> prop
+        -> Property
+    buildCoverage _value _weights _result = property
---
+++ src/test/ValueSpec.hs
@@ -0,0 +1,113 @@
+module ValueSpec where
+
+import Prelude
+
+import Data.Function
+    ( (&) )
+import Data.Group
+    ( Group (..) )
+import Test.Hspec
+    ( Spec, describe, it, parallel )
+import Test.QuickCheck
+    ( Arbitrary, Property, property, (===) )
+import Test.QuickCheck.Classes
+    ( eqLaws
+    , isListLaws
+    , monoidLaws
+    , semigroupLaws
+    , semigroupMonoidLaws
+    , showLaws
+    , showReadLaws
+    )
+import Test.QuickCheck.Classes.Group
+    ( groupLaws )
+import Test.QuickCheck.Classes.Hspec
+    ( testLawsMany )
+import Test.QuickCheck.Classes.Monoid.GCD
+    ( overlappingGCDMonoidLaws )
+import Test.QuickCheck.Classes.Monoid.Monus
+    ( monusLaws )
+import Test.QuickCheck.Classes.Monoid.Null
+    ( monoidNullLaws, positiveMonoidLaws )
+import Test.QuickCheck.Classes.Semigroup.Cancellative
+    ( cancellativeLaws
+    , commutativeLaws
+    , leftCancellativeLaws
+    , leftReductiveLaws
+    , reductiveLaws
+    , rightCancellativeLaws
+    , rightReductiveLaws
+    )
+import Test.QuickCheck.Quid
+    ( Latin (..), Quid )
+import Value
+    ( Balance, Coin, balanceToCoins, coinToBalance )
+
+spec :: Spec
+spec = do
+
+    parallel $ describe "Class instances obey laws" $ do
+        testLawsMany @TestBalance
+            [ commutativeLaws
+            , eqLaws
+            , groupLaws
+            , isListLaws
+            , monoidLaws
+            , monoidNullLaws
+            , semigroupLaws
+            , semigroupMonoidLaws
+            , showLaws
+            , showReadLaws
+            ]
+        testLawsMany @TestCoin
+            [ cancellativeLaws
+            , commutativeLaws
+            , eqLaws
+            , isListLaws
+            , leftCancellativeLaws
+            , leftReductiveLaws
+            , monoidLaws
+            , monoidNullLaws
+            , monusLaws
+            , overlappingGCDMonoidLaws
+            , positiveMonoidLaws
+            , reductiveLaws
+            , rightCancellativeLaws
+            , rightReductiveLaws
+            , semigroupLaws
+            , semigroupMonoidLaws
+            , showLaws
+            , showReadLaws
+            ]
+
+    parallel $ describe "Conversions" $ do
+        it "prop_coinToBalance_balanceToCoins" $
+            prop_coinToBalance_balanceToCoins
+                & property
+        it "prop_coinToBalance_invert_balanceToCoins" $
+            prop_coinToBalance_invert_balanceToCoins
+                & property
+        it "prop_balanceToCoins_coinToBalance_invert" $
+            prop_balanceToCoins_coinToBalance_invert
+                & property
+
+prop_coinToBalance_balanceToCoins :: TestCoin -> Property
+prop_coinToBalance_balanceToCoins c =
+    balanceToCoins (coinToBalance c) === (mempty, c)
+
+prop_coinToBalance_invert_balanceToCoins :: TestCoin -> Property
+prop_coinToBalance_invert_balanceToCoins c =
+    balanceToCoins (invert (coinToBalance c)) === (c, mempty)
+
+prop_balanceToCoins_coinToBalance_invert :: TestBalance -> Property
+prop_balanceToCoins_coinToBalance_invert b =
+    invert (coinToBalance n) <> coinToBalance p === b
+  where
+    (n, p) = balanceToCoins b
+
+type TestBalance = Balance TestAsset
+type TestCoin = Coin TestAsset
+
+newtype TestAsset = TestAsset (Latin Quid)
+    deriving stock (Eq, Ord, Read, Show)
+    deriving Arbitrary via Quid
